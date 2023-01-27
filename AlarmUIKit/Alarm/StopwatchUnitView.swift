34810fb2e5e215627031056ea279c9fc3b2ae7bf
LynnVlasenko <111356109+LynnVlasenko@users.noreply.github.com>
2023-01-27 17:42:15 +0100

    table and line layout
    add small changes to the layout of the table and line

---
+++ AlarmUIKit/Alarm/StopwatchUnitView.swift
@@ -0,0 +1,20 @@
+//
+//  StopwatchUnitView.swift
+//  Alarm
+//
+//  Created by Алина Власенко on 25.01.2023.
+//
+
+import SwiftUI
+
+struct StopwatchUnitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StopwatchUnitView_Previews: PreviewProvider {
+    static var previews: some View {
+        StopwatchUnitView()
+    }
+}
